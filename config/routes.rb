924a024bb2f4f170c5c06e06df00cd7eaa3c01de
oakay455 <ayako110.89@gmail.com>
2022-04-21 14:35:09 +0000

    [Start]Bookers2

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+  get "homes/about" => "homes#about", as: "about"
+  post 'books' => 'books#create'
+
+  resources :books, only: [:new, :create, :show, :index, :destroy, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
